345c2f8a9c4f584d497b908978f7b74a76f48264
jhyang <jk105214@gmail.com>
2024-04-12 15:27:35 +0900

    building dataset  tool

--- src/data.py
+++ src/data.py
@@ -5,7 +5,7 @@ from .feature import composition_to_feature
 from typing import Dict, List
 
 class BaseData:
-    def __init__(self, data:Dict, info_attrs:List[str] = ['id','raw_id','year','feature_type']):
+    def __init__(self, data:Dict={}, info_attrs:List[str] = ['id','raw_id','year']):
         self._info_attrs = info_attrs
         self._feature_attrs = []
         self.device = None
@@ -44,47 +44,68 @@ class BaseData:
     def __dict__(self):
         return self.to_dict()
 
-class CompositionData(BaseData):
-    def __init__(self, data:Dict, composition:Dict, feature_type='basic', **kwargs):
-        super(CompositionData, self).__init__(data, **kwargs)
-        self.feature_type = feature_type
-        self.composition = composition
-        self.comp_feat = composition_to_feature(composition, feature_type=feature_type, **kwargs)
-        self._info_attrs.append('composition')
+class CompData(BaseData):
+    def __init__(self, comp:Dict, data:Dict={}, comp_feat_type:str='active_composit', by_fraction=True, **kwargs):
+        super(CompData, self).__init__(data, **kwargs)
+        self.comp_feat_type = [comp_feat_type, by_fraction]
+        self.comp = comp
+        self.comp_feat = composition_to_feature(comp, feature_type=comp_feat_type, by_fraction=by_fraction)
+        self._info_attrs.extend(['comp', 'comp_feat_type'])
         self._feature_attrs.append('comp_feat')
         self.to_torch()
 
-class ConditionalData1(BaseData):
-    def __init__(self, data:Dict, 
-                 composition:Dict=None, 
-                 metal_composition:Dict={}, 
-                 target_feature_type:str='basic', 
-                 composition_feature_type:str='basic',
-                 metal_feature_type:str='metal_frac',
+class MetalCondLigandData(BaseData):
+    def __init__(self, 
+                 target_comp:Dict,                
+                 data:Dict={},
+                 ligand_comp:Dict=None,
+                 metal_comp:Dict={}, 
+                 ligand_feat_type:str='ligand_composit',
+                 ligand_feat_by_fraction:bool=False,
+                 target_feat_type:str='active_composit',
+                 target_feat_by_fraction:bool=True,
+                 metal_feat_type:str='metal_composit',
+                 metal_feat_by_fraction:bool=False,
                  **kwargs):
-        super(ConditionalData1, self).__init__(data, **kwargs)
+        super(MetalCondLigandData, self).__init__(data, **kwargs)
         
-        target_feat = composition_to_feature(data['target'], feature_type=target_feature_type, **kwargs)
+        if ligand_comp is not None:
+            self.ligand_comp = ligand_comp
+            self.ligand_feat = composition_to_feature(ligand_comp, feature_type=ligand_feat_type, by_fraction=ligand_feat_by_fraction)
+            self.ligand_feat_type = [ligand_feat_type, ligand_feat_by_fraction]
+            metal_comp = {e:f for e,f in ligand_comp.items() if e in MetalElements}
+            self._info_attrs.extend(['ligand_comp', 'ligand_feat_type'])
+            self._feature_attrs.append('ligand_feat')
+        
+        target_feat = composition_to_feature(target_comp, feature_type=target_feat_type, by_fraction=target_feat_by_fraction)
+        element_feat = composition_to_feature(metal_comp, feature_type=metal_feat_type, by_fraction=metal_feat_by_fraction)
 
-        if composition is not None:
-            self.composition = composition
-            self.comp_feat = composition_to_feature(composition, feature_type=composition_feature_type, **kwargs)
-            metal_composition = {e:f for e,f in composition.items() if e in MetalElements}
-            self._feature_attrs.append('comp_feat')
-        element_feat = composition_to_feature(metal_composition, feature_type=metal_feature_type)
+        self.target_comp = target_comp
+        self.target_feat_type = [target_feat_type, target_feat_by_fraction]
+        self.metal_comp = metal_comp
+        self.metal_feat_type = [metal_feat_type, metal_feat_by_fraction]
 
-        self.metal_composition = metal_composition
         self.cond_feat = np.hstack([element_feat, target_feat])
-        self._info_attrs.append('metal_composition')
-        self._feature_attrs.append('cond_feat')
+        self._info_attrs.extend(['target_comp','metal_comp'])
+        self._feature_attrs.append(['cond_feat'])
 
 class BaseDataset(torch.utils.data.Dataset):
     def __init__(self):
         super(BaseDataset, self).__init__()
+        self.init_data()
+
+    def init_data(self):
         self._data = []
-        self._year = []
-        self._element = []
-    
+
+    def read_file(self, data_path):
+        if ('.gz' in data_path) or ('.gzip' in data_path):
+            f = gzip.open(data_path, 'rb')
+        else:
+            f = open(data_path, 'rb')
+        dataset = pickle.load(f)
+        f.close()
+        return dataset
+
     def to_numpy(self):
         for data in self._data:
             data.to_numpy()
@@ -104,21 +125,22 @@ class BaseDataset(torch.utils.data.Dataset):
         return self._data[i]
     
 class CompositionDataset(BaseDataset):
-    def __init__(self):
+    def __init__(self, comp_feat_type='active_composit', by_fraction=True):
         super(CompositionDataset, self).__init__()
+        self._comp_feat_type = comp_feat_type
+        self._by_fraction = by_fraction
 
-    def from_file(self, data_path='../data/unique_data.pkl.gz'):
-        super(CompositionDataset, self).__init__()
-        with gzip.open(data_path, 'rb') as f:
-            dataset = pickle.load(f)
-        
+    def from_file(self, data_path='../data/unique_target.pkl.gz', extend_dataset=False):
+        if not extend_dataset:
+            self.init_data()
+        dataset = self.read_file(data_path)
+
         for data in dataset:
-            comp_data = CompositionData(data, data['target_comp'])
+            comp_data = CompData(data=data, comp=data['target_comp'], comp_feat_type=self._comp_feat_type, by_fraction=self._by_fraction)
             self._data.append(comp_data)
-            self._element.append(comp_data.comp_feat != 0)
-        self._data = np.array(self._data)
         self._year = np.array([d.year for d in self._data])
-        self._element = np.vstack(self._element)
+        self.to_torch()
+        self.to('cpu')
 
     def cfn(self, dataset):
         feat = []
@@ -127,3 +149,41 @@ class CompositionDataset(BaseDataset):
             feat.append(data.comp_feat)
             info.append(data.to_dict())
         return torch.vstack(feat), info
+
+class ConditionDataset(BaseDataset):
+    def __init__(self, 
+                 ligand_feat_type:str='ligand_composit',
+                 target_feat_type:str='active_composit',
+                 metal_feat_type:str='metal_composit',
+    ):
+        super(ConditionDataset, self).__init__()
+        self._ligand_feat_type = ligand_feat_type
+        self._target_feat_type = target_feat_type
+        self._metal_feat_type = metal_feat_type
+
+    def from_file(self, data_path='../data/unique_target.pkl.gz', extend_dataset=False):
+        if not extend_dataset:
+            self.init_data()
+        dataset = self.read_file(data_path)
+
+        for data in dataset:
+            for prec_data in data['precursors']:
+                MetalCondLigandData(data=data, 
+                                    prec_data['precursor_comp'],
+                )
+
+    def from_targets(self, target_comps:List[Dict], extend_dataset=False):
+        if not extend_dataset:
+            self.init_data()
+        for target_comp in target_comps:
+            self.from_target(target_comp, True)
+
+    def from_target(self, target_comp:Dict, extend_dataset=True):
+        if not extend_dataset:
+            self.init_data()
+        for ele, frac in target_comp.items():
+            if ele not in MetalElements:
+                continue
+            MetalCondLigandData(info_attrs=[],
+                                data=  {ele:frac}
+
--- src/feature.py
+++ src/feature.py
@@ -13,7 +13,10 @@ for k in ['cgcnn','elemnet','magpie','magpie_sc','mat2vec','matscholar','megnet1
 
 composit_fnc = ['active_composit','metal_composit','ligand_composit']
 
-def composition_to_feature(composit_dict, feature_type='active_composit', dtype=np.float32, enable_fraction=True):
+def composition_to_feature(composit_dict, 
+                           feature_type='active_composit', 
+                           dtype=np.float32, 
+                           by_fraction=True):
     '''
     composit_dict = {
         element_0 (str): fraction_0 (float),
@@ -22,7 +25,12 @@ def composition_to_feature(composit_dict, feature_type='active_composit', dtype=
     }
     '''
     if feature_type in composit_fnc:
-        return eval(f'{feature_type}_feature(composit_dict, dtype, enable_fraction)')
+        if feature_type == 'active_composit':
+            return active_composit_feature(composit_dict, dtype, by_fraction)
+        elif feature_type == 'metal_composit':
+            return metal_composit_feature(composit_dict, dtype, by_fraction)
+        elif feature_type == 'ligand_composit':
+            return ligand_composit_feature(composit_dict, dtype, by_fraction)
     elif feature_type in elmd.keys():
         vec = []
         norm = 1.0 / np.sum(list(composit_dict.values()))
@@ -31,34 +39,34 @@ def composition_to_feature(composit_dict, feature_type='active_composit', dtype=
                 if PRINT_EXCEPTION_WARNING is not None:
                     print(PRINT_EXCEPTION_WARNING.format(ele, feature_type))
                 continue
-            vec.append(np.array(elmd[feature_type][ele]) * (frac if enable_fraction else 1))
+            vec.append(np.array(elmd[feature_type][ele]) * (frac if by_fraction else 1))
         PRINT_EXCEPTION_WARNING = None
         return (np.sum(vec, 0) * norm).astype(dtype)
     else:
         raise TypeError(f'feature type [{feature_type}] is not supported', composit_fnc + list(elmd.keys()))
 
-def active_composit_feature(composit_dict, dtype, enable_fraction=True, **kwargs):
+def active_composit_feature(composit_dict, dtype, by_fraction=True, **kwargs):
     feat_vec = np.zeros(len(ActiveElements), dtype=dtype)
     for ele, frac in composit_dict.items():
         if frac < NEAR_ZERO: continue
-        feat_vec[ActiveElements.index(ele)] = frac if enable_fraction else 1
+        feat_vec[ActiveElements.index(ele)] = frac if by_fraction else 1
     feat_vec /= feat_vec.sum()
     return feat_vec
 
-def metal_composit_feature(composit_dict, dtype, enable_fraction=True, **kwargs):
+def metal_composit_feature(composit_dict, dtype, by_fraction=True, **kwargs):
     feat_vec = np.zeros(len(MetalElements) + 1, dtype=dtype)
     for ele, frac in composit_dict.items():
-        feat_vec[MetalElements.index(ele) + 1] = frac if enable_fraction else 1
+        feat_vec[MetalElements.index(ele) + 1] = frac if by_fraction else 1
     if feat_vec.sum() == 0:
         feat_vec[0] = 1
     return feat_vec
 
-def ligand_composit_feature(composit_dict, dtype, enable_fraction=True, **kwargs):
+def ligand_composit_feature(composit_dict, dtype, by_fraction=True, **kwargs):
     feat_vec = np.zeros(len(LigandElements) + 1, dtype=dtype)
     feat_vec[0] = 1
     for ele, frac in composit_dict.items():
         if ele in MetalElements:
             feat_vec[0] = 0
             continue
-        feat_vec[LigandElements.index(ele) + 1] = frac if enable_fraction else 1
+        feat_vec[LigandElements.index(ele) + 1] = frac if by_fraction else 1
     return feat_vec

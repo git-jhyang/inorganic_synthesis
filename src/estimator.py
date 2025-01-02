eca1eaa42a8d89115da18e1efdc36ab35b3bbf54
jhyang <jk105214@gmail.com>
2025-01-03 00:18:44 +0900

    sampling test dataset

--- src/estimator.py
+++ src/estimator.py
@@ -8,21 +8,23 @@ with open('../data/estimator.pkl','rb') as f:
 def estimate_temperature(target_composition, temperature_range=[500,2500], n_points=401):
     xs = np.linspace(*temperature_range, n_points)
     feat = np.hstack([
-        composition_to_feature(target_composition, feature_type='elemnet'),
+        composition_to_feature(target_composition, feature_type='comp'),
+#        composition_to_feature(target_composition, feature_type='elemnet'),
         composition_to_feature(target_composition, feature_type='cgcnn')
     ])
-    pca_feat = target_pca.transform([feat])
-    data = np.vstack(np.hstack([[x], pca_feat]) for x in xs).T
+    pca_feat = target_pca.transform(feat).reshape(-1)
+    data = np.vstack([np.hstack([[x], pca_feat]) for x in xs]).T
     ys = kde_temperature(data)
     return xs, ys
 
 def estimate_time(target_composition, time_range=[500,2500], n_points=401):
     xs = np.linspace(*time_range, n_points)
     feat = np.hstack([
-        composition_to_feature(target_composition, feature_type='elemnet'),
+        composition_to_feature(target_composition, feature_type='comp'),
+#        composition_to_feature(target_composition, feature_type='elemnet'),
         composition_to_feature(target_composition, feature_type='cgcnn')
     ])
-    pca_feat = target_pca.transform([feat])
-    data = np.vstack(np.hstack([[x], pca_feat]) for x in xs).T
+    pca_feat = target_pca.transform(feat).reshape(-1)
+    data = np.vstack([np.hstack([[x], pca_feat]) for x in xs]).T
     ys = kde_time(data)
     return xs, ys
